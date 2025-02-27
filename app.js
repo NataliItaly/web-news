45d4872bdf867f891529779cb21da1de4916898e
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-02-27 19:32:33 +0100

    feat: add active nav link

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 import RouterHandler from "./router.js";
 
+window.addEventListener("hashchange", function (e) {
+  const hash = window.location.hash; //e.newURL.split("#")[1];
+  //console.log("window hash", window.location.hash);
+  const headerLinks = document.querySelectorAll(".header__link");
+  console.log(headerLinks);
+  headerLinks.forEach((link) => {
+    link.classList.remove("header__link_active");
+    if (link.getAttribute("href") === `#${hash}`) {
+      link.classList.add("header__link_active");
+    }
+  });
+});
+
 class App {
   constructor() {
     console.log("work");
